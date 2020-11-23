90def657fd6f459066869c28e64a4f5d0cac7c78
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-11-23 18:04:56 +0330

    use of created class

--- EcmaScript/Lesson 03/L1.js
+++ EcmaScript/Lesson 03/L1.js
@@ -1,8 +1,15 @@
 class Human{
 
+    firstName;
+    lastName;
+    age;
 }
 
 
 const obj1 = new Human();
+obj1.firstName = "hadi";
+obj1.lastName = "Mohammadian";
+obj1.age = 37 ;
 
 console.log(obj1);
+console.log(`My firstname is : ${obj1.firstName} and last name is : ${obj1.lastName} and my age is : ${obj1.age}`);
